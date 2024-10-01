3f4e4a15d89e0d11691373dc4187aa01705ab92f
Nacho <isanccal@myuax.com>
2024-10-02 01:47:52 +0200

    al

--- Método Bisección/biseccion.py
+++ Método Bisección/biseccion.py
@@ -1,3 +1,14 @@
+import math
+
+def f(c):
+  g = 9.81
+  m = 68.1
+  t = 10
+  v = 40
+  return (g*m/c)*(1-math.exp(-(c/m)*t))-v
+
+
+
 def biseccion(x1,x2,e,f):             
   if f(x1)*f(x2) > 0:
     print('x1 y x2 no tiene sentido')                   
@@ -7,6 +18,8 @@ def biseccion(x1,x2,e,f):
   an = x2
   anmenos1 = x1
   error = abs(an - anmenos1)
+  
+  
   while error > e:
 
     a = (x1+x2)/2
@@ -15,4 +28,7 @@ def biseccion(x1,x2,e,f):
     elif f(a)*f(x2) < 0:
       x2 = a
     anmenos1 = a
+
+  
+print(biseccion(0.1,5,0.1,f))
 
